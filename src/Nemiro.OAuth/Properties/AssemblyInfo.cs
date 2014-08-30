051cf0c4e7b47da2ba686068b9cb2a76ceddaafe
Aleksey Nemiro <aleksey@kbyte.ru>
2014-08-30 17:35:48 +0400

    v1.1.x.375

--- src/Nemiro.OAuth/Properties/AssemblyInfo.cs
+++ src/Nemiro.OAuth/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.4.367")]
-[assembly: AssemblyFileVersion("1.1.4.367")]
+[assembly: AssemblyVersion("1.1.4.375")]
+[assembly: AssemblyFileVersion("1.1.4.375")]
