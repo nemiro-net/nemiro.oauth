e0e48cfc3fd063d52a8f2c94f698e60666801a54
Aleksey Nemiro <aleksey@kbyte.ru>
2015-02-23 13:14:17 +0300

    Registration multiple clients with identical name
    https://github.com/alekseynemiro/nemiro.oauth.dll/issues/4#issuecomment-75467656

---
+++ src/AspNetWebFormsMulticlients/ExternalLoginResult.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ExternalLoginResult.aspx.cs" Inherits="AspNetWebFormsMulticlients.ExternalLoginResult" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head id="Head1" runat="server">
+    <title>Login Result</title>
+  </head>
+  <body>
+    <form id="form1" runat="server">
+      <div id="page">
+        <h2>Login Result</h2>
+        <pre id="preResult" runat="server" />
+        <hr />
+        <asp:HyperLink ID="hlTryAgain" runat="server" Text="Try again" NavigateUrl="~/" />
+      </div>
+    </form>
+
+		<script type="text/javascript">
+			if (window.location.search.indexOf('popup=true') != -1) {
+				// set results to main window
+				window.opener.location = window.location;
+				// close pop-up window
+				window.close();
+			}
+		</script>
+  </body>
+</html>
---
+++ src/AspNetWebFormsMulticlients/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("AspNetWebFormsMulticlients")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AspNetWebFormsMulticlients")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для COM-компонентов.  Если необходим доступ к типу в этой сборке из 
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+[assembly: Guid("c57e6eed-e7c2-4a67-a8fe-1da547194a97")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
